96e4930dcf74a4bdbbab0ba9c3892b3695458a56
livecodealex <s1505883@sms.ed.ac.uk>
2017-10-22 23:25:21 +0100

    fixed travis file

--- app/src/main/java/com/songle/s1505883/songle/MainActivity.java
+++ app/src/main/java/com/songle/s1505883/songle/MainActivity.java
@@ -2,14 +2,38 @@ package com.songle.s1505883.songle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import tools.DebugMessager;
 
 public class MainActivity extends Activity
 {
+    private static DebugMessager console = DebugMessager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void settingsClicked(View view)
+    {
+        console . info("Settings Clicked");
+    }
+
+    public void playClicked(View view)
+    {
+        console . info("Play clicked");
+    }
+
+    public void wordlistClicked(View view)
+    {
+        console . info("wordlist clicked");
+    }
+
+    public void guessedClicked(View view)
+    {
+        console.info("guessed clicked");
     }
 }
